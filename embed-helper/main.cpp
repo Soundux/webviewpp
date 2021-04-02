9cbb8277ab1489d634f997a8b7bee0ae4f6a135c
Curve <fynnbwdt@gmail.com>
2021-04-02 15:54:38 +0200

    feat: update embed-helper

--- embed-helper/main.cpp
+++ embed-helper/main.cpp
@@ -1,8 +1,10 @@
+#include <algorithm>
 #include <filesystem>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main(int argc, char **args)
 {
@@ -14,37 +16,57 @@ int main(int argc, char **args)
 
     if (std::filesystem::exists(args[1]))
     {
-        std::filesystem::recursive_directory_iterator iterator(args[1]);
-        std::ofstream output("embedded.hpp");
+        std::filesystem::create_directory("embedded");
+        std::ofstream output("embedded/webview_base.hpp");
         output << "#pragma once" << std::endl
                << "#include <map>" << std::endl
                << "#include <string>" << std::endl
                << "#include <vector>" << std::endl
-               << "inline const std::map<const std::string, const std::vector<unsigned char>> embedded_files = {";
+               << "inline std::map<const std::string, const std::vector<unsigned char>> embedded_files;";
+        output.close();
+        std::vector<std::string> filesToInclude;
+
+        std::filesystem::recursive_directory_iterator iterator(args[1]);
 
         for (const auto &file : iterator)
         {
             if (file.path().filename().string().at(0) == '.' || !file.is_regular_file())
                 continue;
 
-            output << "{\"" << file.path().filename().string() << "\", {";
+            filesToInclude.emplace_back(file.path().filename().string() + ".hpp");
+            auto fileFunc = file.path().filename().string();
+            std::replace(fileFunc.begin(), fileFunc.end(), '.', '_');
+            std::replace(fileFunc.begin(), fileFunc.end(), '-', '_');
 
-            std::ifstream fileStream(file.path(), std::ios::binary);
-            std::vector<unsigned char> buffer(std::istreambuf_iterator<char>(fileStream), {});
+            std::ofstream fileStream("embedded/" + file.path().filename().string() + ".hpp");
+            fileStream << "#pragma once" << std::endl
+                       << "#include \"webview_base.hpp\"" << std::endl
+                       << "auto webview_embed_file_" << fileFunc << " = []() -> bool { embedded_files.insert({\""
+                       << file.path().filename().string() << "\", {";
+
+            std::ifstream fileDataStream(file.path(), std::ios::binary);
+            std::vector<unsigned char> buffer(std::istreambuf_iterator<char>(fileDataStream), {});
 
             for (auto it = buffer.begin(); it != buffer.end(); it++)
             {
-                output << "0x" << std::setw(2) << std::setfill('0') << std::hex << static_cast<unsigned int>(*it);
+                fileStream << "0x" << std::setw(2) << std::setfill('0') << std::hex << static_cast<unsigned int>(*it);
                 if (std::distance(it, buffer.end()) > 1)
                 {
-                    output << ",";
+                    fileStream << ",";
                 }
             }
 
-            output << "}}," << std::endl;
+            fileStream << "}}); return true; }();";
+            fileStream.close();
         }
 
-        output << "};";
+        std::ofstream includeFile("embedded/include.hpp");
+        includeFile << "#pragma once" << std::endl << "#include \"webview_base.hpp\"" << std::endl;
+        for (const auto &file : filesToInclude)
+        {
+            includeFile << "#include \"" + file << "\"" << std::endl;
+        }
+        includeFile.close();
     }
     else
     {
