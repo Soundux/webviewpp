7708ce87e0841c395e10121dcdf3ff90f0adaf41
Curve <fynnbwdt@gmail.com>
2021-04-23 20:41:08 +0200

    docs: document header

--- webview/include/core/basewindow.hpp
+++ webview/include/core/basewindow.hpp
@@ -62,37 +62,64 @@ namespace Webview
         virtual BaseWindow &operator=(const BaseWindow &) = delete;
         BaseWindow(std::string identifier, std::size_t width, std::size_t height);
 
+        /// \effects Hides the window
         virtual void hide();
+        /// \effects Shows the window
         virtual void show();
+        /// \returns Whether or the window is hidden
         virtual bool isHidden();
 
+        /// \effects Sets the window size
         virtual void setSize(std::size_t, std::size_t);
+        /// \returns The width and height in form of an `std::pair`
         virtual std::pair<std::size_t, std::size_t> getSize();
 
+        /// \returns The title of the window
         virtual std::string getTitle();
+        /// \effects Sets the window title
         virtual void setTitle(std::string);
 
+        /// \effects Runs the mainloop
+        /// \remarks Is blocking
         virtual void run() = 0;
+        /// \effects Closes the webview
         virtual void exit() = 0;
 
+        /// \returns The current url
         virtual std::string getUrl();
+        /// \effects Navigates to the given url
         virtual void setUrl(std::string);
 
+        /// \effects Enables the context menu
         virtual void enableContextMenu(bool);
+        /// \effects Enables the developer tools
         virtual void enableDevTools(bool) = 0;
 
+        /// \effects Exposes the given function
+        /// \remarks If the given Function is an `AsyncFunction` it will be run in a new thread
         void expose(const Function &);
+        /// \effects Calls the given javascript function
+        /// \returns The result of the javascript function call as `T`
+        /// \preconditions `T` must be serializable by nlohmann::json
+        /// \remarks You should never call `.get()` on the returned future in a **non async** context as it will
+        /// freeze the webview
         template <typename T> std::future<T> callFunction(JavaScriptFunction &&function)
         {
             auto future = callFunction(std::forward<JavaScriptFunction>(function)).getResult();
             return std::async(std::launch::async, [future] { return future.get().get<T>(); });
         }
 
+        /// \effects Runs the given javascript code
         virtual void runCode(const std::string &) = 0;
+        /// \effects Makes the given javascript code run on document load
         virtual void injectCode(const std::string &) = 0;
 
+        /// \effects Sets the close-callback to the given callback
+        /// \remarks If the callback returns `true` the webview will not close
         virtual void setCloseCallback(std::function<bool()>);
+        /// \effects Sets the navigate-callback to the given callback
         virtual void setNavigateCallback(std::function<void(const std::string &)>);
+        /// \effects Sets the resize-callback to the given callback
         virtual void setResizeCallback(std::function<void(std::size_t, std::size_t)>);
     };
 } // namespace Webview
